35c976119c8113a716da69ab27cd988f3b0b1921
Vince Carey <stvjc@channing.harvard.edu>
2021-04-26 17:17:42 -0400

    run_app option

--- R/process_artifacts.R
+++ R/process_artifacts.R
@@ -3,10 +3,15 @@
 #' @param startup logical(1) defaults to TRUE; passed to `status_db_init`
 #' @param serialize_pnet logical(1) if TRUE emits list of pkgnet outputs as RDS
 #' @param pnet_obj_name character(1) defaults to "pnet.rds"
+#' @param run_app logical(1) defaults to TRUE; runs `browse_checks` on available data
 #' @note Starts a SQLite connection.  The connection is closed in the call to dbConnect, this
 #' should be conditioned with an on.exit here in case the connection was not closed.
+#' If run_app is FALSE, the connection is closed on exit.
+#' This function will create a SQLite database and list of pkgnet objects for use
+#' with browse_checks.
 #' @export
-process_artifacts = function (dbpath = tempfile(), startup=TRUE, serialize_pnet = TRUE, pnet_obj_name = "pnet.rds") 
+process_artifacts = function (dbpath = tempfile(), run_app=TRUE, 
+      startup=TRUE, serialize_pnet = TRUE, pnet_obj_name = "pnet.rds") 
 {
     bcc = lapply(dir(patt = "bcch"), readRDS)
     bn = lapply(dir(patt = "bcch"), function(x) gsub("_.*", "", 
@@ -23,8 +28,9 @@ process_artifacts = function (dbpath = tempfile(), startup=TRUE, serialize_pnet 
     status_db_init(dbpath, bchkdfs, TRUE)
     con = dbConnect(RSQLite::SQLite(), dbpath)
     if (serialize_pnet) 
-        saveRDS(ppp, file = "pnet.rds", compress = "xz")
+        saveRDS(ppp, file = pnet_obj_name, compress = "xz")
     # run on.exit with a check for openness of con FIXME
-    browse_checks(con, ppp)
+    if (run_app) browse_checks(con, ppp)
+    else dbDisconnect(con)
 }
 
