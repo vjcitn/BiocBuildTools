de1dee242628c466b28e349cae7d02f4afb53488
Vince Carey <stvjc@channing.harvard.edu>
2021-04-26 23:02:02 -0400

    remove pkgnet for test

--- inst/app2/app.R
+++ inst/app2/app.R
@@ -2,7 +2,7 @@ library(BiocBuildTools)
 library(RSQLite)
 con = RSQLite::dbConnect(RSQLite::SQLite(), system.file(
    "sqlite/vjc3.sqlite", package="BiocBuildTools"))
-suppressWarnings({
-pk = readRDS(system.file("pkgnet/pnet.rds", package="BiocBuildTools"))
-})
-BiocBuildTools::browse_checks(con, pk)
+#suppressWarnings({
+#pk = readRDS(system.file("pkgnet/pnet.rds", package="BiocBuildTools"))
+#})
+BiocBuildTools::browse_checks(con, NULL)
