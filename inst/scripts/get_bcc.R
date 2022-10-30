1894ecf0c4aec164175f319196a03d4e2260de65
Vincent Carey <stvjc@visually-civil-zebra.js2local>
2022-10-30 18:20:31 +0000

    revised get_bcc/checks

--- inst/scripts/get_bcc.R
+++ inst/scripts/get_bcc.R
@@ -4,12 +4,10 @@ library(BiocBuildTools)
 library(rcmdcheck)
 library(BiocCheck)
 
-if (!dir.exists("bcchecks")) dir.create("bcchecks")
-if (!dir.exists("bcobj")) dir.create("bcobj")
 
-sources.folder = "~/BBS_space/REL_316_srcs"
-bcchecks.destination = "./bcchecks"
-bcobj.destination = "./bcobj"
+sources.folder = "./srcs316_2"
+bcchecks.destination = "./bcchecks_2"
+bcobj.destination = "./bcobj_2"
 
 library(BiocParallel)
 spar = SnowParam(30, threshold="DEBUG")
@@ -27,14 +25,14 @@ done = gsub(".BiocCheck$", "", dir(bcchecks.destination))
 todo = setdiff(goal, done)
 print(head(todo))
 print(length(todo))
-#todo = setdiff(todo, "a4") # a4 causes great difficulties
 todopaths = paste0(sources.folder, "/", todo)
 shuffle = function(x) sample(x, size=length(x), replace=FALSE)
-allbc3 = bplapply(shuffle(todopaths[1:10]), function(x) {
+allbc3 = bplapply(shuffle(todopaths), function(x) {
    print(x)
    futile.logger::flog.info(paste0("'x' = ", x))
+   futile.logger::flog.error(paste0("'x' = ", x))
    ans = try(bco2df(BiocCheck::BiocCheck(x, checkDir=bcchecks.destination))) 
-   dest = paste0("./bcobj/", paste0(basename(x), "_chk.rds"))
+   dest = paste0(bcobj.destination, "/", paste0(basename(x), "_chk.rds"))
    saveRDS(ans, dest)
    NULL
    })
--- inst/scripts/get_checks.R
+++ inst/scripts/get_checks.R
@@ -1,60 +1,56 @@
 
 # RUN WITH xvfb-run
    
-   
-   
    library(BiocParallel)
    library(rcmdcheck)
    library(BiocBuildTools)
+
+checks.destination = "./chks316_2"
+sources.folder = "./srcs316_2"
+
 spar = SnowParam(35)
 bplog(spar) = TRUE
-bplogdir(spar) = "~/BBS_space/BPLOGS"
+bplogdir(spar) ="./BPLOGS"
 bptimeout(spar) = 600
 bpthreshold(spar) = "DEBUG"
 
    register(spar)
+
+#register(SerialParam())
+source("build_exp_ps.R")
+
+pset = readRDS("PackageSet_3.16d.rds")
    
-   checks.destination = "~/BBS_space/chks316"
-   sources.folder = "~/BBS_space/REL_316_srcs"
+pset@pkgnames = pset@pkgnames[1:70]
+
+
+populate_local_gits(pset, sources.folder)
 
 if (!dir.exists(checks.destination)) dir.create(checks.destination)
 if (!dir.exists(sources.folder)) stop("can't find sources in sources.folder; folder does not exist")
 
-#source("build_packageset_3.16a.R")
-
-pset = readRDS("PackageSet_3.16a.rds")	
-
-#populate_local_gits(pset, sources.folder)
-
-   
-get_checks = function(pkgset, sources.folder, checks.destination, N.TRIES=5) {
+get_checks = function(pkgset, sources.folder, checks.destination) {
    
    allp = slot(pkgset, "pkgnames")
    N_SRCS = length(dir(sources.folder))
    N_DONE = length(dir(checks.destination))
-   done_pkgs = gsub("_chk.rds", "", dir(checks.destination))
+   done_pkgs = character()
+   if (N_DONE > 0) done_pkgs = gsub("_chk.rds", "", dir(checks.destination))
    
    shuffle = function(x) sample(x, size=length(x), replace=FALSE)
    
    cur = 0
    todo <- setdiff(allp, done_pkgs)
-   while(length(todo)>0) {
      print(length(todo))
      print(head(todo))
-     od = getwd()
-     setwd(sources.folder)
-     print(getwd())
-     tryall = bplapply(shuffle(todo), function(x) {
+     tryall = bplapply(paste0(sources.folder, "/", shuffle(todo)), function(x) {
           futile.logger::flog.info(paste0("'x' = ", x))
-          z = try(safe_rcmdcheck(x)); 
-          saveRDS(z, paste0(checks.destination, "/", x, "_chk.rds"))
+          z = try(rcmdcheck::rcmdcheck(x, error_on="never")) # try(safe_rcmdcheck(x)); 
+          futile.logger::flog.error(paste0("'x' = ", x))
+          saveRDS(z, paste0(checks.destination, "/", basename(x), "_chk.rds"))
+          NULL
           })
-     cur = cur+1
-     if (cur > N_TRIES) break
-     done_pkgs = gsub("_chk.rds", "", dir(checks.destination))
-     todo <- setdiff(allp, done_pkgs)
    }
           
-  } 
 
 get_checks(pset, sources.folder, checks.destination)
